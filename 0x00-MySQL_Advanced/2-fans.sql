f892c8faf8a0ebca15421fe420344aa724d7a9cb
AbdullahKhames <abdullahkhames96@gmail.com>
2024-01-19 01:17:45 +0200

    create table with unique

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,4 @@
 --Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, fans as nb_fans FROM metal_bands;
+SELECT origin, fans as nb_fans, dense_rank() 
+OVER ( order by mark desc ) 
+AS 'dense_rank' FROM metal_bands;
