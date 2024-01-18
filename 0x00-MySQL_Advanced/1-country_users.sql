415a2268baf79e5e448386997ed4fb20bec6db8f
AbdullahKhames <abdullahkhames96@gmail.com>
2024-01-19 00:54:14 +0200

    create table with unique

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create table with unique index
+CREATE TABLE users IF NOT EXISTS contacts
+( id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(30) NOT NULL,
+  name VARCHAR(25),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+  CONSTRAINT email_unique UNIQUE (email)
+);
--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++
@@ -1,7 +0,0 @@
--- create table with unique index
-CREATE TABLE users IF NOT EXISTS contacts
-( id INT PRIMARY KEY AUTO_INCREMENT,
-  email VARCHAR(30) NOT NULL,
-  name VARCHAR(25),
-  CONSTRAINT email_unique UNIQUE (email)
-);
