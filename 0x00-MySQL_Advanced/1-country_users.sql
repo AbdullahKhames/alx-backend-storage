f7d2cb391501cf58d6c2138fd14749aae233652f
AbdullahKhames <abdullahkhames96@gmail.com>
2024-01-19 01:01:56 +0200

    create table with unique

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- create table with unique index
-CREATE TABLE users IF NOT EXISTS contacts
-( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users 
+( id INT PRIMARY KEY AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL,
   name VARCHAR(255),
   country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
