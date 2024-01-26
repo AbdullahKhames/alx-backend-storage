2df39e97568dbece407442f4b12a7c5099dadd02
AbdullahKhames <abdullahkhames96@gmail.com>
2024-01-26 22:34:55 +0200

    list all from pymongo

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module to list all in python
+"""
+
+
+def func():
+    """function to do the module work
+    """
+    pass
+
+
+if __name__ == '__main__':
+    """to prevent code execution when importing
+    """
+    pass
--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module to list all in python
+"""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """function to do the module work
+    """
+    data = []
+    for s in mongo_collection.find():
+        data.append(s)
+    return data
+
+
+if __name__ == '__main__':
+    """to prevent code execution when importing
+    """
+    pass
