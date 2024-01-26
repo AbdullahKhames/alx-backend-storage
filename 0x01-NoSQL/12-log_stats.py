ca42a21186ab82a3b98ab478112f1bb9f0f52320
AbdullahKhames <AbdullahKhames96@gmail.com>
2024-01-26 13:34:04 -0800

    methods

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 """module to list all in python
 """
+from pymongo import MongoClient
 
 
-def func():
-    """function to do the module work
-    """
-    pass
+client = MongoClient("mongodb://localhost:27017/")
+nginx = client.logs.nginx
+print(f"{nginx.count_documents({})} logs")
+print("Methods:")
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for method in methods:
+    print(
+        "\tmethod {}: {}".format(
+            method, nginx.count_documents({"method": method}))
+    )
 
-
+print(
+    "{} status check".format(
+        nginx.count_documents({"method": "GET", "path": "/status"}))
+)
 if __name__ == '__main__':
     """to prevent code execution when importing
     """
