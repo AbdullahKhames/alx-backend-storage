f45ca4a19b8b5e63a4f6e80414e59c21a66793cd
AbdullahKhames <abdullahkhames96@gmail.com>
2024-01-26 22:44:31 +0200

    insert_one function pymongo

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,10 +3,10 @@
 """
 
 
-def func():
+def insert_school(mongo_collection, **kwargs):
     """function to do the module work
     """
-    pass
+    return mongo_collection.insert_one(kwargs).inserted_id
 
 
 if __name__ == '__main__':
